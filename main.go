53efb33b87ffa7731e26a3e5b025ff6808359ac9
Mariano Sosto <sostomariano@gmail.com>
2021-10-24 02:18:00 -0300

    setup commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/davecgh/go-spew/spew"
+	"github.com/joho/godotenv"
+	"log"
+	"time"
+)
+
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		t := time.Now()
+		genesisBlock := Block{0, t.String(), 0, "", ""}
+		spew.Dump(genesisBlock)
+		Blockchain = append(Blockchain, genesisBlock)
+	}()
+	log.Fatal(run())
+
+}
+
