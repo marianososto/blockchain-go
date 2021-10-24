09ee1374f272c033a240e3eb6aeff97df8d10cf3
Mariano Sosto <sostomariano@gmail.com>
2021-10-24 18:57:29 -0300

    add tcp connections for connecting peers and add PoW algorithm

--- blockchain.go
+++ blockchain.go
@@ -3,22 +3,26 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
 	"strconv"
+	"strings"
 	"time"
 )
 
 var Blockchain []Block
 
 type Block struct {
-	Index     int
-	Timestamp string
-	BPM       int
-	Hash      string
-	PrevHash  string
+	Index      int
+	Timestamp  string
+	BPM        int
+	Hash       string
+	PrevHash   string
+	Difficulty int
+	Nonce      string
 }
 
 func calculateHash(block Block) string {
-	record := strconv.Itoa(block.Index) + block.Timestamp + strconv.Itoa(block.BPM) + block.PrevHash
+	record := strconv.Itoa(block.Index) + block.Timestamp + strconv.Itoa(block.BPM) + block.PrevHash + block.Nonce
 	h := sha256.New()
 	h.Write([]byte(record))
 	hashed := h.Sum(nil)
@@ -32,7 +36,23 @@ func generateBlock(oldBlock Block, BPM int) (Block, error) {
 	newBlock.Timestamp = time.Now().String()
 	newBlock.BPM = BPM
 	newBlock.PrevHash = oldBlock.Hash
-	newBlock.Hash = calculateHash(newBlock)
+	//newBlock.Hash = calculateHash(newBlock)
+	newBlock.Difficulty = oldBlock.Difficulty
+
+	for i := 0; ; i++ {
+		hex := fmt.Sprintf("%x", i)
+		newBlock.Nonce = hex
+		newHash := calculateHash(newBlock)
+		if isHashValid(newHash, newBlock.Difficulty) {
+			fmt.Println(newHash + " not valid hash.")
+			time.Sleep(time.Duration(500) * time.Millisecond) // to simulate the PoW duration
+			continue
+		} else {
+			fmt.Println(newHash + " work is done")
+			newBlock.Hash = newHash
+			break
+		}
+	}
 
 	return newBlock, nil
 }
@@ -59,3 +79,11 @@ func isBlockValid(newBlock, oldBlock Block) bool {
 	return true
 }
 
+func getLastBlock() Block {
+	return Blockchain[len(Blockchain)-1]
+}
+
+func isHashValid(hash string, difficulty int) bool {
+	prefix := strings.Repeat("0", difficulty)
+	return strings.HasPrefix(hash, prefix)
+}
--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/joho/godotenv"
+	"io"
 	"log"
+	"net"
+	"os"
+	"strconv"
 	"time"
 )
 
+// bcServer handles incoming concurrent blocks.
+var bcServer chan []Block
 
 func main() {
 	err := godotenv.Load()
@@ -14,13 +22,74 @@ func main() {
 		log.Fatal(err)
 	}
 
-	go func() {
-		t := time.Now()
-		genesisBlock := Block{0, t.String(), 0, "", ""}
-		spew.Dump(genesisBlock)
-		Blockchain = append(Blockchain, genesisBlock)
-	}()
-	log.Fatal(run())
+	bcServer = make(chan []Block)
 
+	t := time.Now()
+	genesisBlock := Block{0, t.String(), 0, "", "",1,""}
+	spew.Dump(genesisBlock)
+	Blockchain = append(Blockchain, genesisBlock)
+
+	server, err := net.Listen("tcp", ":"+os.Getenv("ADDR"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer server.Close()
+
+	for {
+		conn, err := server.Accept()
+		fmt.Println("Received new connection")
+		if err != nil {
+			log.Fatal(err)
+		}
+		go handleConn(conn)
+	}
 }
 
+func handleConn(conn net.Conn) {
+	defer func() {
+		fmt.Println("\nClosing connection")
+		conn.Close()
+	}()
+	io.WriteString(conn, "Enter a new BPM:")
+	scanner := bufio.NewScanner(conn)
+
+	for scanner.Scan() {
+		bpm, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Printf("%v not a number: %v", scanner.Text(), err)
+			continue
+		}
+		newBlock, err := generateBlock(getLastBlock(), bpm)
+		if err != nil {
+			log.Printf("Failed generating new block. error:%v", err)
+			continue
+		}
+		if isBlockValid(newBlock, getLastBlock()) {
+			newBlockchain := append(Blockchain, newBlock) //TODO:danger concurrently modifying a shared variable
+			replaceChain(newBlockchain)
+		}
+		fmt.Printf("\nAdded new blockchain with index:%d and BPM:%d", newBlock.Index, bpm)
+		//bcServer <- Blockchain
+		io.WriteString(conn, "\nEnter a new BPM:")
+	}
+
+	/*
+		go func() {
+			every30Secs := time.NewTicker(time.Duration(30) * time.Second)
+			defer every30Secs.Stop()
+
+			for {
+				select {
+				case <-every30Secs.C:
+					output, err := json.Marshal(Blockchain)
+					if err != nil {
+						log.Fatal(err)
+						return // end goroutine
+					}
+					io.WriteString(conn, string(output))
+					break
+				}
+			}
+		}()
+	*/
+}
